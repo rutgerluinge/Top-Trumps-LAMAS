5881a4d0fd55a51908715f4023905a6e056202b2
firevos <thomasjvos12@gmail.com>
2023-05-23 21:47:50 +0200

    First working version of the game
    A first draft of a working version of the game Top Trumps, with just a few cards available. Each player randomly choses which stat to play. No knowledge info implemented yet.

---
+++ cfg.py
@@ -0,0 +1,10 @@
+# Set parameters here.
+
+# Global parameters. TODO: set adjustable on front end.
+PLAYER_COUNT = 2
+STATS_COUNT = 2 # Equal to or lower than the amount of stats specified in cards.json
+CARD_COUNT = 6 # Will be overwritten if there are less cards declared in cards.json
+
+# Behind the screens parameters
+RANDOM_SEED = 1
+DEBUG = True
